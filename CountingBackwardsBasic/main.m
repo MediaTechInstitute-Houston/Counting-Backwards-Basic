18c244ac6a4370439c14c7ed62c57f68c74f98c0
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-05-12 13:10:12 -0500

    Initial Commit

---
+++ CountingBackwardsBasic/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CountingBackwardsBasic
+//
+//  Created by Mark Dumes on 5/12/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
