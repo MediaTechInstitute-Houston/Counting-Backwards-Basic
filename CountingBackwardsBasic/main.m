9724c682dede7307b099ea5ce036baf850d15ac5
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-11-20 11:33:25 -0600

    Learning Loops
    While loop, for loop, or do while loop

--- CountingBackwardsBasic/main.m
+++ CountingBackwardsBasic/main.m
@@ -5,7 +5,7 @@
 //  Created by Mark Dumes on 5/12/14.
 //  Copyright (c) 2014 CollegeBlink. All rights reserved.
 //
-
+// Purpose is to figure out how to count backwards from 100 and output the numbers
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[])
@@ -13,10 +13,12 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
+    //1st step(create varibles)
+        int n = 100;
+    //2nd step(while loop)
+        for (n=100; n>=0; n--) {
+            NSLog(@"%i",n);
+        }    }
     return 0;
 }
 
